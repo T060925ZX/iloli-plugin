0d6abaec18afb75c89e69be8bed6109b9e86a0f7
T060925ZX <t1602833550@163.com>
2025-04-19 21:22:27 +0800

    新增tkupdate类以支持iloli-plugin的更新功能

---
+++ apps/update.js
@@ -0,0 +1,22 @@
+import { update } from "../../other/update.js"
+export class tkupdate extends plugin {
+  constructor() {
+    super({
+      name: "[萝莉插件]更新",
+      event: "message",
+      priority: 1145,
+      rule: [
+        {
+          reg: "^#*(i|萝莉)(插件)?(强制)?更新$",
+          fnc: "update"
+        }
+      ]
+    })
+  }
+  async update(e = this.e) {
+    e.msg = `#${e.msg.includes("强制")?"强制":""}更新iloli-plugin`
+    const up = new update(e)
+    up.e = e
+    return up.update()
+  }
+}
