2edf132c52cfb44b03c77d1f877999a01bd83863
T060925ZX <t1602833550@163.com>
2025-04-19 22:50:38 +0800

    删除未使用的yaml模块导入

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import yaml from 'yaml'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
