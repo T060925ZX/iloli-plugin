8b5f218864e0f1672e47b986e459bc4c14ffab19
T060925ZX <t1602833550@163.com>
2025-04-26 20:18:55 +0800

    细节调整

--- apps/moyu.js
+++ apps/moyu.js
@@ -1,29 +1,31 @@
 import Cfg from '../model/Cfg.js'
+import { segment } from 'oicq' 
 
 export class moyu extends plugin {
     constructor() {
-      super({
-        name: "摸鱼日历",
-        dsc: "摸会鱼",
-        event: "message",
-        priority: -1,
-        rule: [
-          {
-            reg: `^#?(摸鱼|摸鱼日历)$`,
-            fnc: "moyu"
-          }
-        ]
-      })
-      this.config = Cfg.getConfig('api');
-      this.moyu = this.config?.moyu || 'https://api.vvhan.com/api/moyu';
+        super({
+            name: "摸鱼日历",
+            dsc: "获取摸鱼日历图片",
+            event: "message",
+            priority: -1,
+            rule: [
+                {
+                    reg: `^#?(摸鱼|摸鱼日历)$`, 
+                    fnc: "getMoyuImage"     
+                }
+            ]
+        });
+
+        this.config = Cfg.getConfig('api') || {};
+        this.moyuApi = this.config.moyu || 'https://api.vvhan.com/api/moyu'; 
     }
 
-  async moyu(e) {
-    try {
-        await this.reply(segment.image(this.config));
-    } catch (error) {
-        await this.reply('获取摸鱼图片失败');
+    async getMoyuImage(e) {
+        try {
+            await this.reply(segment.image(this.moyuApi), { quote: true , at: false });
+        } catch (error) {
+            console.error("获取摸鱼图片失败:", error);
+            await this.reply("摸鱼失败，API请求出错啦~");
+        }
     }
-}
-
 }
