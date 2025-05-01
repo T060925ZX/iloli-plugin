de2f32c1893bcf58a6c44c15e57d7329c37c19de
T060925ZX <t1602833550@163.com>
2025-05-01 13:01:44 +0800

    每日60s

--- apps/moyu.js
+++ apps/moyu.js
@@ -12,12 +12,17 @@ export class moyu extends plugin {
                 {
                     reg: `^#?(摸鱼|摸鱼日历)$`, 
                     fnc: "getMoyuImage"     
+                },
+                {
+                    reg: `^#?(今日|每日)?日报$`, 
+                    fnc: "get60sImage"     
                 }
             ]
         });
 
         this.config = Cfg.getConfig('api') || {};
         this.moyuApi = this.config.moyu || 'https://api.vvhan.com/api/moyu'; 
+        this.mrApi = this.config.moyu || 'http://zj.v.api.aa1.cn/api/60s-v2/?cc=SaDuck'; 
     }
 
     async getMoyuImage(e) {
@@ -28,4 +33,14 @@ export class moyu extends plugin {
             await this.reply("摸鱼失败，API请求出错啦~");
         }
     }
+
+    async get60sImage(e) {
+        try {
+            await this.reply(segment.image(this.moyuApi), { quote: true , at: false });
+        } catch (error) {
+            console.error("获取摸鱼图片失败:", error);
+            await this.reply("摸鱼失败，API请求出错啦~");
+        }
+    }
+    
 }
